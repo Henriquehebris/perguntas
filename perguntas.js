134b72f9fba86b3b006ccecf60507c7376ad5b7b
Henrique <00001138367588sp@al.educacao.sp.gov.br>
2024-11-06 11:01:19 -0300

    up
    up

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'Quem descobriu o Brasil?',
+    'Pedro Álvares Cabral (1467-1520) como o primeiro navegante a chegar em terras brasileiras, junto com uma frota de 13 navios e mais de mil marujos.'
+)
+
+criaCartao(
+    'Quimica',
+    'Quantos elementos existem na tabela periodica?',
+    'Atualmente são 118 elementos químicos classificados e 90 deles são os elementos naturais'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da China?',
+    'A capital da China é Pequim'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'O que é uma estrofe?',
+    'divisão de um texto lírico ou épico, formada por um determinado número de versos que apresentam ou não rima entre si; estância'
+)
